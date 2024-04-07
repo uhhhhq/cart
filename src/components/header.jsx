0c6e218beffb498e39bc53767ad27dd09632e835
barbashovad232@mail.ru <khananna42@gmail.com>
2024-04-07 18:34:01 +0300

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+       <nav className="page-header #aa00ff purple accent-4">
+          <div className="container">
+             <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">
+                  Fortnite Shop
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                   <li>
+                      <a href="!#">Repository</a>
+                   </li>
+                </ul>
+             </div>
+          </div>
+       </nav>
+    );  
+ }
+ export {Header};
