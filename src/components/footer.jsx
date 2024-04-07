3d13f6030c6b702efa242bda39056100d15ba22e
uhhhhq <121134621+uhhhhq@users.noreply.github.com>
2024-04-07 19:00:54 +0300

    footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
        <footer className="page-footer #aa00ff purple accent-4">
           <div className="footer-copyright">
              <div className="container">
-                by zanozzzza © {new Date().getFullYear()}
+                by uhq © {new Date().getFullYear()}
                 <a className="grey-text text-lighten-4 right" href="#!">
                    Repository
                 </a>
