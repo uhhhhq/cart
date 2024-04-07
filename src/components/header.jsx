534a62444d7d683669d63e1465898e3f40a170f6
uhhhhq <121134621+uhhhhq@users.noreply.github.com>
2024-04-07 19:01:40 +0300

    header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ function Header() {
        <nav className="page-header #aa00ff purple accent-4">
           <div className="container">
              <div className="nav-wrapper">
-                <a href="!#" className="brand-logo">
+                <a href="https://github.com/uhhhhq/cart" className="brand-logo">
                   Fortnite Shop
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
